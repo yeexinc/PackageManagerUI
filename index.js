1d676bd6debaae4d40bcf18637cd96155174fff2
aparajit-pratap <aparajit.pratap@autodesk.com>
2017-04-11 14:07:28 +0800

    add SearchBar component

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ app.get("/", function(req, res) {
     res.render("index.html");
 });
 
+app.get("/packages", function(req, res) {
+  loadJsonFromFile("./resources/data/packages.json", req, res);
+});
+
 app.listen(8080, function() {
     console.log("Services listening on port 8080");
 });
--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,16 +1,17 @@
 import * as React from "react";
 
+import { SearchFilter } from "./SearchFilter";
+
 export interface SearchBarProps {
     searchString : string
 }
 
 export class SearchBar extends React.Component<SearchBarProps, undefined> {
+
     render() {
         return (<div className="SearchBarContainer">
             <div className="SearchBar">{this.props.searchString}
-                <div className="SearchFilter">
-
-                </div>
+                <SearchFilter/>
             </div>
         </div>);
     }
