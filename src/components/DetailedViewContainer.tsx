34ac54274c0b40657460cc9f914be08989c9cbb5
Ben Goh <benglin@outlook.com>
2017-04-11 15:40:21 +0800

    Added package fetching to detailed view

--- src/components/DetailedViewContainer.tsx
+++ src/components/DetailedViewContainer.tsx
@@ -1,12 +1,51 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 
+import { PackageVersionDetail } from "./PackageVersionDetail";
+
 export interface DetailedViewProps {
     packageId: string
 }
 
-export class DetailedViewContainer extends React.Component<DetailedViewProps, undefined> {
+export interface DetailedViewState {
+    packageJsonDownloaded: boolean
+}
+
+export class DetailedViewContainer extends React.Component<DetailedViewProps, DetailedViewState> {
+
+    activePackageJson: any = null;
+
+    constructor(props: DetailedViewProps) {
+        super(props);
+
+        this.state = {
+            packageJsonDownloaded: false
+        }
+
+        this.beginDownloadPackage = this.beginDownloadPackage.bind(this);
+    }
+
+    beginDownloadPackage(packageId: string) {
+
+        fetch("/package/" + packageId)
+            .then(function (response: Response) {
+
+                return response.text();
+
+            }).then(function (jsonString) {
+
+                this.activePackageJson = JSON.parse(jsonString);
+
+            });
+    }
+
     render() {
-        return (<div>{ this.props.packageId }</div>);
+        if (!this.state.packageJsonDownloaded) {
+            return (<div>Select package to download...</div>);
+        }
+
+        let dependencies = ["jaz", "juice"];
+        let content = this.activePackageJson.content;
+        return (<PackageVersionDetail changeLog="foo" content={content} dependencies={dependencies} />);
     }
 }
